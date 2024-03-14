0a7bed3f763e4a036d1676183f85305fbe1fb7a1
meruuuuooo <merudeibitto@gmail.com>
2024-03-14 13:15:57 +0800

    Fix bug in search function

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,7 +1,7 @@
 const resultEl = document.getElementById("results");
 
 function search(search_value) {
-  if (search_value == "" || search_value == null) { // fix the bug when search value is empty or null no result will be shown
+  if (search_value == "" || search_value == null || search_value == " ") { // fix the bug when search value is empty or null no result will be shown
     $("#results").html("");
     return;
   } else {
